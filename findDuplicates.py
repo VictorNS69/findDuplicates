f30a3837e297bf177604c681fc642a31b7f9deb6
int-0 <tobias.deb@gmail.com>
2018-02-10 20:06:41 +0100

    Little improves and more OOP

--- findDuplicates.py
+++ findDuplicates.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 __author__ = "Victor Nieves Sanchez"
 __copyright__ = "Copyright 2018, Victor Nieves Sanchez"
@@ -16,19 +16,101 @@ import sys
 import hashlib
 import glob
 from os.path import getsize, basename
+import os.path
+import logging
+
+_DEB = logging.debug
+_ERR = logging.error
+
+class FileInfo(object):
+    def __init__(self, filename):
+        if not os.path.isfile(filename):
+            raise OSError('File not found: %s' % filename)
+        self.__fileName = filename
+
+    @property
+    def name(self):
+        return self.__fileName
+
+    @property
+    def path(self):
+        return os.path.abspath(self.name)
+
+    @property
+    def size(self):
+        return os.path.getsize(os.path.abspath(self.name))
+
+    @property
+    def md5(self):
+        return hashlib.md5(open(self.name, 'rb').read()).hexdigest()
+
+    def __eq__(self, other_file):
+        ''' self == other_file '''
+        if other_file.size != self.size:
+            return False
+        if other_file.md5 != self.md5:
+            return False
+        return True
+
+    def __ne__(self, other_file):
+        ''' self != other_file '''
+        return not self.__eq__(other_file)
+
+    def __str__(self):
+        return self.name
 
 
-class TargetFile(object):
-    fileName = ""
-    filePath = ""
-    fileSize = 0
-    fileMd5 = ""
+class FileSet(object):
+    '''Computes file list to allow quick search'''
+    def __init__(self, root_folder, recursive=False):
+        self.__root = root_folder
+        self.__recursive = recursive
+        self._rebuild_file_list_()
 
-    def __init__(self, file):
-        self.fileName = file
-        self.filePath = os.path.abspath(file)
-        self.fileSize = os.path.getsize(os.path.abspath(file))
-        self.fileMd5 = hashlib.md5(open(self.filePath, 'rb').read()).hexdigest()
+    @property
+    def root(self):
+        return self.__root
+
+    @root.setter
+    def root(self, new_root):
+        self.__root = new_root
+        self._rebuild_file_list_()
+
+    @property
+    def files(self):
+        return self.__candidates
+
+    def _rebuild_file_list_(self):
+        self.__candidates = []
+        self._make_candidates_(self.__recursive)
+
+    def _make_candidates_(self, recursive):
+        for filename in glob.glob(os.path.join(self.root, '**' if recursive else '*'), recursive=recursive):
+            filename = os.path.abspath(filename)
+            if os.path.islink(filename):
+                continue
+            _DEB('Adding file %s' % filename)
+            self.__candidates.append(filename)
+
+    def search_by_size(self, size):
+        result = list(filter(lambda f: os.path.getsize(f) == size, self.files))
+        _DEB('Files with size %s found: %s' % (size, ','.join(result)))
+        return result
+
+    def __assert_exists__(self, filename):
+        filename = os.path.abspath(filename)
+        if filename not in self.files:
+            raise ValueError('File %s not in filelist' % filename)
+        return filename
+    
+    def get_fileinfo(self, filename):
+        filename = self.__assert_exists__(filename)
+        return FileInfo(filename)
+
+    def remove(self, filename):
+        filename = self.__assert_exists__(filename)
+        self.__candidates.remove(filename)
+
 
 def parse():
     parser = argparse.ArgumentParser(description='search if there are duplicate files of the desired file.')
@@ -36,56 +118,50 @@ def parse():
     parser.add_argument('path', nargs='?', default=os.getcwd(), action="store", help='source path')
     parser.add_argument('-r', '--recursive', default=False, action='store_true', help='search into directories',
                         dest='recursive')
+    parser.add_argument('-p', '--pretty-output', default=False, action='store_true', help='show pretty output',
+                        dest='pretty_output')
+    parser.add_argument('--debug', default=False, action='store_true', help='be verbose',
+                        dest='debug')
+    parser.add_argument('--version', action='version', version=__version__)
+    
     return parser.parse_args()
 
-'''Recursive'''
-def candidatesR (targetFile, path):
-    print ("RECURSIVE SEARCH")
-    candidates = []
-    for file in glob.glob(path +'/**', recursive =True):
-        if targetFile.fileSize == os.path.getsize (file) and\
-                        targetFile.fileMd5 == hashlib.md5(open(file, 'rb').read()).hexdigest() and\
-                        targetFile.filePath != os.path.abspath(file) and not os.path.islink(file):
-            candidates.append(os.path.abspath(file))
-    return candidates
 
-'''Non-recursive'''
-def candidatesNoR(targetFile, path):
-    candidates = []
-    print ("NON-RECURSIVE SEARCH")
-    for file in glob.glob(path+'/*', recursive=False):
-        if targetFile.fileSize == os.path.getsize(file) and \
-                        targetFile.fileMd5 == hashlib.md5(open(file, 'rb').read()).hexdigest() and\
-                        targetFile.filePath != os.path.abspath(file) and not os.path.islink(file):
-            candidates.append(os.path.abspath(file))
-    return candidates
+def show_pretty(duplicates):
+    if len(duplicates) > 0:
+        print('Duplicates files found:')
+        for f in duplicates:
+            print('\t%s' % f)
 
-def checkName (targetFile, fileList):
-    if len(fileList) == 0:
-        print ("No duplicated files.")
-    else:
-        for file in fileList:
-            if targetFile.fileName == os.path.basename(file):
-                print ("Duplicated file (SAME NAME) in: " + os.path.abspath(file))
-            else:
-                print ("Duplicated file (DIFFERENT NAME) in: " + os.path.abspath(file))
 
 def main():
     userData = parse()
     try:
-        tf = TargetFile(userData.file)
-    except:
-        print("ERROR: ", sys.exc_info()[0])
+        target = FileInfo(userData.file)
+    except OSError as e:
+        _ERR("Error reading file %s: %s" % (userData.file, e))
         sys.exit(1)
-    files = []
-    if userData.recursive:
-        files = candidatesR(tf, userData.path)
-    elif not userData.recursive:
-        files = candidatesNoR(tf, userData.path)
+        
+    files = FileSet(userData.path, userData.recursive)
+    try:
+        files.remove(target.name)
+    except ValueError:
+        pass
+        
+    candidates = [FileInfo(f) for f in files.search_by_size(target.size)]
+    equals = list(filter(lambda f: f == target, candidates))
 
-    checkName(tf, files)
+    if userData.pretty_output:
+        show_pretty(equals)
+    else:
+        # Show output
+        for f in equals:
+            print(str(f))
     sys.exit(0)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(message)s',
+        level=logging.DEBUG if '--debug' in sys.argv else logging.INFO)
     main()
