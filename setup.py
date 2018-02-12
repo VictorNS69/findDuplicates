6773e05d360646277145a67e94a49dfd424be1fc
Víctor Nieves <vnievess@gmail.com>
2018-02-12 16:20:22 +0100

    Add files via upload
    Still not working

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+__AUTHOR__ = "Victor Nieves Sanchez"
+__COPYRIGHT__ = "Copyright 2018, Victor Nieves Sanchez"
+__CREDITS__ = ["Victor Nieves Sanchez", "Tobias Diaz"]
+__LICENSE__ = "GPL"
+__VERSION__ = "3.0.0"
+__PYTHON__= "3.6.4"
+__EMAIL__ = "vnievess@gmail.com"
+
+
+from distutils.core import setup
+from setuptools import setup
+
+setup(name='Find Duplicates',
+	version=__VERSION__,
+	description='Search if there are duplicate files of the desired file.',
+	author=__AUTHOR__,
+	author_email=__EMAIL__,
+	url='https://github.com/VictorNS69',
+	license=__LICENSE__,
+	packages=['main'],
+	packages_dir={'main': 'src'},)
